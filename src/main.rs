72d1a9b74d6046cc069aff1657c78f7a99c5c709
Frixxie <freddahf@outlook.com>
2019-12-05 20:50:16 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+extern crate reqwest;
+use serde_json::Value;
+use std::fs;
+
+fn main() {
+    let loc = fs::read_to_string("/home/fredrik/projects/weather/location").unwrap();
+    let api = fs::read_to_string("/home/fredrik/projects/weather/openweather-api").unwrap();
+
+    loc.trim_matches(char::is_control).to_string();
+    api.trim_matches(char::is_control).to_string();
+
+    let url = format!(
+        "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}",
+        loc, api
+    );
+
+    let res = reqwest::get(url.as_str()).unwrap().text().unwrap();
+
+    //println!("{}", res);
+
+    let v: Value = serde_json::from_str(res.as_str()).unwrap();
+
+    println!(
+        "{},{},{},{},{},{},{},{},{},{},{},{},{},{}",
+        v["dt"],
+        v["name"],
+        v["sys"]["country"],
+        v["weather"][0]["description"],
+        v["sys"]["sunrise"],
+        v["sys"]["sunset"],
+        v["clouds"]["all"],
+        v["wind"]["speed"],
+        v["wind"]["deg"],
+        v["main"]["temp_min"],
+        v["main"]["temp_max"],
+        v["main"]["temp"],
+        v["main"]["humidity"],
+        v["main"]["pressure"],
+    );
+
+    //println!("{:#?}", res);
+}
