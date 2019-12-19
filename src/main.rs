596307bd5f91ff67175572891933f29ebccacbaa
Frixxie <freddahf@outlook.com>
2019-12-19 23:02:36 +0100

    Added support for more locations

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,45 @@ use serde_json::Value;
 use std::fs;
 
 fn main() {
-    let loc = fs::read_to_string("/home/fredrik/projects/weather/location").unwrap();
+    let locs = fs::read_to_string("/home/fredrik/projects/weather/locations").unwrap();
     let api = fs::read_to_string("/home/fredrik/projects/weather/openweather-api").unwrap();
 
-    loc.trim_matches(char::is_control).to_string();
+    locs.trim_matches(char::is_control).to_string();
     api.trim_matches(char::is_control).to_string();
 
-    let url = format!(
-        "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}",
-        loc, api
-    );
+    for loc in locs.split_whitespace() {
+        //println!("{}", loc);
 
-    let res = reqwest::get(url.as_str()).unwrap().text().unwrap();
+        let url = format!(
+            "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}",
+            loc, api
+        );
 
-    //println!("{}", res);
+        let res = reqwest::get(url.as_str()).unwrap().text().unwrap();
 
-    let v: Value = serde_json::from_str(res.as_str()).unwrap();
+        //println!("{}", res);
 
-    println!(
-        "{},{},{},{},{},{},{},{},{},{},{},{},{},{}",
-        v["dt"],
-        v["name"],
-        v["sys"]["country"],
-        v["weather"][0]["description"],
-        v["sys"]["sunrise"],
-        v["sys"]["sunset"],
-        v["clouds"]["all"],
-        v["wind"]["speed"],
-        v["wind"]["deg"],
-        v["main"]["temp_min"],
-        v["main"]["temp_max"],
-        v["main"]["temp"],
-        v["main"]["humidity"],
-        v["main"]["pressure"],
-    );
+        let v: Value = serde_json::from_str(res.as_str()).unwrap();
+
+        println!(
+            "{},{},{},{},{},{},{},{},{},{},{},{},{},{}",
+            v["dt"],
+            v["name"],
+            v["sys"]["country"],
+            v["weather"][0]["description"],
+            v["sys"]["sunrise"],
+            v["sys"]["sunset"],
+            v["clouds"]["all"],
+            v["wind"]["speed"],
+            v["wind"]["deg"],
+            v["main"]["temp_min"],
+            v["main"]["temp_max"],
+            v["main"]["temp"],
+            v["main"]["humidity"],
+            v["main"]["pressure"],
+        );
+
+    }
 
     //println!("{:#?}", res);
 }
